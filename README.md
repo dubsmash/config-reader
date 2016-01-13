74a78d2117c002d61297724ddc6cdf52339b3ac3
Tim Specht <tim@tim-specht.de>
2016-01-13 20:34:51 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
-# config-reader
-A Python JSON-file config reader
+## Config Reader
+
+Config parser is a nice little helper enabling you to automatically load different configuration values from various sources including .json files and the OS environment in a unified way.
+It was initially created to help clean up configuration values for Django projects and make settings easily overridable but can also be used in various other use cases.
+
+[![Build Status](https://travis-ci.org/dubsmash/config-parser.png?branch=master)](https://travis-ci.org/dubsmash/config-parser)
+[![Downloads](https://pypip.in/download/config-parser/badge.png)](https://pypi.python.org/pypi/config-parser/)
+[![Latest Version](https://pypip.in/version/config-parser/badge.png)](https://pypi.python.org/pypi/config-parser/)
+
+
+## Requirements
+
+* Tested for python 2.7
+
+## Installation:
+
+    pip install config-parser
+
+## Development
+
+Running the tests:
+
+    
+    $ pip install -r requirements_testing.txt
+    $ py.test tests --pep8 --flakes
+
+## Developing new features
+
+Every new feature should be:
+
+* Documented
+* Tested
+* Implemented
+* Pushed to main repository
+
+### How to write documentation
+
+When new feature implementation starts you should place it into `development version` pull. Add `Development version`
+section to `Release notes` and describe every new feature in it. Use `#anchors` to facilitate navigation.
+
+Every feature should have title and information that it was implemented in current development version.
+
+
+## Publishing new releases
+
+Increment version in `config_reader/__init__.py`. For example:
+
+    __version__ = '0.1.1'  # from 0.1
+
+Move to new version section all release notes in documentation.
+
+Add date for release note section.
+
+Run tests.
+
+Commit changes with message "Version 0.1.1"
+
+Add new tag version for commit:
+
+    $ git tag 0.1.1
+
+Push to master with tags:
+
+    $ git push origin master --tags
+
+Publish to pypi:
+
+    $ python setup.py publish
---
+++ tests/tests.py
@@ -0,0 +1,84 @@
+import os
+
+from pyflakes.test.harness import TestCase
+import pytest
+from config_reader import reader
+
+from config_reader.exceptions import ConfigTypeCastError, \
+    ConfigKeyNotFoundError
+
+
+class TestConfigReaderFunctionality(TestCase):
+    """
+    Tests the various functionalities in the ConfigReader class
+    """
+    def setUp(self):
+        """
+        Creates a config object and populates it with a list of dicts,
+        where each dict is either loaded from a JSON file
+        or is a custom dictionary (e.g os.environ)
+        """
+        self.config = reader.ConfigReader([os.environ,
+                                           "tests/static/test_config.json"])
+
+    def test_get_boolean_type_variable_from_config(self):
+        """
+        Tests for the presence of a variable in the configs list
+        which has a value of 'true'
+        """
+        key = "use_anonymous"
+        value = self.config.get_boolean(key)
+        self.assertEqual(type(value), bool)
+        self.assertEqual(value, True)
+
+    def test_gets_boolean_type_for_valid_representation(self):
+        """
+        Tests for the presence of a variable in the configs list
+        which has a value 'T', which should represent a TRUE value.
+        """
+        key = "bool_setting"
+        value = self.config.get_boolean(key)
+        self.assertEqual(type(value), bool)
+        self.assertEqual(value, True)
+
+    def test_raises_exception_for_incorrect_type_expected(self):
+        """
+        Test that asserts for a ConfigTypeCastError being raised
+        when trying to get a non floating point variable
+        """
+        with pytest.raises(ConfigTypeCastError):
+            self.config.get_float("host")
+
+    def test_variable_present_in_os(self):
+        """
+        Tests that a variable which is not present in the JSON file,
+        but has been added to the OS env, exists
+        """
+        import os
+        os.environ["SECRET_KEY"] = "topsecret"
+        value = self.config.get_string("SECRET_KEY")
+        self.assertEqual(value, "topsecret")
+
+    def test_non_existant_variable_lookup_raises_error(self):
+        """
+        Tests that trying to get a non existant variable in the configs list,
+        raises a ConfigKeyNotFoundError.
+        """
+        with pytest.raises(ConfigKeyNotFoundError):
+            self.config.get_string("SOMEMONKEYVALUE")
+
+    def test_optional_variable_returns_none(self):
+        """
+        Test that trying to get a non existant variable
+        with optional set to False returns None
+        """
+        value = self.config.get_string("NON_EXISTANT", optional=True)
+        self.assertEqual(value, None)
+
+    def test_for_string_separated_by_newline(self):
+        """
+        Test that we can get a list of strings from a string
+        present in the config,separated by newlines
+        """
+        value = self.config.get_string_list("list_of_strings")
+        self.assertGreater(len(value), 1)
